746fac77255b96dffcf7bd5705f89b80479822b4
yanyao <zdc666nb@gmail.com>
2023-08-04 17:25:49 +0800

    小修小补

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  important: true,
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
